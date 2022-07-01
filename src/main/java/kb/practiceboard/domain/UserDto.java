474f83ee353b1a04855c2d93c618b88124b87a08
BeenKimKr <bin9610@gmail.com>
2022-07-01 20:52:36 +0900

    Fix: UserDto 일부 수정

--- src/main/java/kb/practiceboard/domain/UserDto.java
+++ src/main/java/kb/practiceboard/domain/UserDto.java
@@ -31,7 +31,8 @@ public class UserDto {
   private String nickname;
 
   @JsonInclude(JsonInclude.Include.NON_NULL)
-  @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$", message = "비밀번호는 영문과 특수문자를 포함해 8자 이상 20자 이하여야 합니다.")
+  @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$",
+      message = "비밀번호는 영문과 특수문자를 포함해 8자 이상 20자 이하여야 합니다.")
   private String password;
 
   @JsonInclude(JsonInclude.Include.NON_NULL)
